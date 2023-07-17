c744ad851a7db6a1a85b99af90dd4d167394fa3a
PhongNhat43 <128346856+PhongNhat43@users.noreply.github.com>
2023-07-17 16:26:13 +0700

    Add files via upload

---
+++ BacSi_App/BacSi_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BacSi_App
+//
+//  Created by devsenior on 17/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
